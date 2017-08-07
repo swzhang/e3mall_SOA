b66c8afccaafdd2a0de2733776813d375049f9c2
swzhang <swzhang.gis@qq.com>
2017-08-07 16:57:47 +0800

    增加商品类目选择功能getItemCatList

---
+++ e3-manager/e3-manager-interface/src/main/java/com/e3mall/service/ItemCatService.java
@@ -0,0 +1,9 @@
+package com.e3mall.service;
+
+import java.util.List;
+
+import com.e3mall.common.pojo.TreeNode;
+
+public interface ItemCatService {
+	List<TreeNode> getItemCatList(long parentId);
+}
---
+++ e3-manager/e3-manager-service/src/main/java/com/e3mall/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,39 @@
+package com.e3mall.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.e3mall.common.pojo.TreeNode;
+import com.e3mall.mapper.TbItemCatMapper;
+import com.e3mall.pojo.TbItemCat;
+import com.e3mall.pojo.TbItemCatExample;
+import com.e3mall.pojo.TbItemCatExample.Criteria;
+import com.e3mall.service.ItemCatService;
+
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+
+	public List<TreeNode> getItemCatList(long parentId) {
+		// 根据parentId查询列表
+		TbItemCatExample example = new TbItemCatExample();
+		// 设置查询条件
+		Criteria criteria = example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+		List<TbItemCat> list = itemCatMapper.selectByExample(example);
+		List<TreeNode> treeNodes = new ArrayList<>();
+		for (TbItemCat tbItemCat : list) {
+			TreeNode treeNode = new TreeNode();
+			treeNode.setId(tbItemCat.getId());
+			treeNode.setText(tbItemCat.getName());
+			treeNode.setState(tbItemCat.getIsParent()?"closed":"open");
+			treeNodes.add(treeNode);
+		}
+		return treeNodes;
+	}
+
+}
